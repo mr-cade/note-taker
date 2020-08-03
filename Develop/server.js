6d31a481ffffe656135e265b9d2051f7554ed005
mr-cade <cadelpetersen@gmail.com>
2020-08-03 15:26:26 -0600

    api get routes from db

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var fs = require("fs")
 
 // Sets up the Express App
 // =============================================================
@@ -12,18 +13,32 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
+// Routes
+// =============================================================
+// Basic routes that send the user to the notes home page and notes edit page
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
-  });
-
-app.get("/notes", function(req, res) {
-res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// api routes
+var notes = require("./db/db.json");
+// Displays all notes
+app.get("/api/notes", function(req, res) {
+    return res.json(notes);
+});
+// post routes
+app.post("/api/notes", function(req, res) {
+    notes.push(req.body)
+    res.json(true);
+})
+
 
 
 // listens for server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
-  });
-  
+});
