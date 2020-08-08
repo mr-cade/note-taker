2d877e26153578521dbb21a901bfaddcab8e1b34
mr-cade <cadelpetersen@gmail.com>
2020-08-08 17:11:10 -0600

    refactoring and comments

--- server.js
+++ server.js
@@ -3,10 +3,6 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
-var myJson = require("./db/db.json");
-let notes = [{
-}];
-
 
 // Sets up the Express App
 // =============================================================
@@ -19,7 +15,6 @@ app.use(express.json());
 
 // HTML Routes
 // =============================================================
-// Basic routes that send the user to the notes home page and notes edit page
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
@@ -36,21 +31,8 @@ app.get("/assets/css/styles.css", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/assets/css/styles.css"));
 });
 
-// // API Routes
-// // =============================================================
-// // Displays all notes
-// app.get("/api/notes", function(req, res) {
-//     res.json(notes);
-// });
-
-// // post routes
-// app.post("/api/notes", function(req, res) {
-//     notes.push(req.body)
-//     res.json(true);
-//     console.log(res.location);
-// });
-// =================================================================
-// API's
+// API Routes
+// =============================================================
 app.get("/api/notes", function (req, res) {
     fs.readFile(__dirname + "/db/db.json", function (err, data) {
         if (err) throw err;
@@ -59,43 +41,38 @@ app.get("/api/notes", function (req, res) {
     })
 });
 
-
 app.post("/api/notes", function (req, res) {
-
     fs.readFile(__dirname + "/db/db.json", function (err, data) {
         if (err) throw err;
         let notes = JSON.parse(data);
         var newNote = req.body;
+        // time stamp for unique note id
         var timeStamp = new Date();
         newNote.id = timeStamp;
         notes.push(newNote);
 
         fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), function (err, data) {
             if (err) throw err;
-            console.log(newNote);
             res.json(newNote);
         })
     })
 });
 
 app.delete("/api/notes/:id", function (req, res) {
-    //get the id out of the req.
+    //get time stamp id and convert to string 
     let notesID = req.params.id.toString();
-    console.log(notesID);
-    //get all the notes out of the database.
+    //retrieve notes from JSON
     fs.readFile(__dirname + "/db/db.json", function (err, data) {
         if (err) throw err;
         let notes = JSON.parse(data);
+        // filter out selected time stamp id
         let filteredNotes = notes.filter((note) => { return note.id !== notesID })
-        console.log(filteredNotes);
         fs.writeFile(__dirname + "/db/db.json", JSON.stringify(filteredNotes), function (err, data) {
             if (err) throw err;
-            
             res.json(filteredNotes);
         })
     });
 });
-
 
 // listens for server
 app.listen(PORT, function () {
